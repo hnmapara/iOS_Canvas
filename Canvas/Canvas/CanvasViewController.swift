5ece0b250c1933097c407ffa65f7b2f0d8c50fc5
Harshit Mapara <mapara@yahoo-inc.com>
2016-11-02 20:29:11 -0700

    Milestone 3 : pan and tap gesture with animation to open/close tray

--- Canvas/Canvas/CanvasViewController.swift
+++ Canvas/Canvas/CanvasViewController.swift
@@ -15,10 +15,24 @@ class CanvasViewController: UIViewController {
     var trayOriginalCenter: CGPoint!
     var gestureOriginalCenter: CGPoint!
     
+    var openedLocation : CGPoint!
+    var closedLocation : CGPoint!
+    
+    var velociyOfTray : CGPoint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //var heightDiff : CGFloat! = self.view.frame.height - trayView.frame.size.height
+        let closedLocationY = trayView.frame.height/2.0 + self.view.frame.height - 35.0
+        let openLocationY = self.view.frame.height - self.trayView.frame.height/2
+        
+        openedLocation = CGPoint(x: trayView.center.x, y: openLocationY)
+        closedLocation = CGPoint(x: trayView.center.x, y: closedLocationY)
+        
+        trayView.center = closedLocation
+        
         // Do any additional setup after loading the view.
     }
 
@@ -50,16 +64,55 @@ class CanvasViewController: UIViewController {
             // self.trayOriginalCenter = point
         } else if sender.state == .changed {
             print("Gesture changed at: \(point)")
+            velociyOfTray = sender.velocity(in: self.view)
             let translation = CGPoint(x: point.x-self.gestureOriginalCenter.x, y: point.y-self.gestureOriginalCenter.y)
             self.trayView.center = CGPoint(x: self.trayOriginalCenter.x, y: self.trayOriginalCenter.y + translation.y)
         } else if sender.state == .ended {
             print("Gesture ended at: \(point)")
+            if velociyOfTray.y > 0 {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                        self.trayView.center = self.closedLocation
+                    }, completion: { (Bool) in
+                        
+                })
+//                UIView.animate(withDuration: 0.5, animations: {
+//                    self.trayView.center = self.closedLocation
+//                })
+            } else if velociyOfTray.y < 0 {
+//                UIView.animate(withDuration: 0.5, animations: {
+//                    self.trayView.center = self.openedLocation
+//                })
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                        self.trayView.center = self.openedLocation
+                    }, completion: { (Bool) in
+                        
+                })
+
+            }
+            
         }
         
     }
     
     
-    
+    @IBAction func onTapGesture(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            if trayView.center.y > self.view.frame.height {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                    self.trayView.center = self.openedLocation
+                    }, completion: { (Bool) in
+                        
+                })
+            } else {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                    self.trayView.center = self.closedLocation
+                    }, completion: { (Bool) in
+                        
+                })
+            }
+
+        }
+    }
     
     
     
